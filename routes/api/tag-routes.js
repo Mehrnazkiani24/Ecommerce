30f1ec7393f153b3bcef4797b6e8b344905d24c2
mehrnazkiani24 <mehrnaz03@yahoo.com>
2024-06-02 14:24:05 -0700

    updated and added tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -21,11 +21,11 @@ router.get('/:id', async(req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
       // JOIN with travellers, using the Trip through table
-      include: [{ model: Traveller, through: Trip, as: 'location_travellers' }]
+      include: [{ model: Product, through: ProductTag, }]
     });
 
     if (!tagData) {
-      res.status(404).json({ message: 'No location found with this id!' });
+      res.status(404).json({ message: 'No Tag Found!' });
       return;
     }
 
@@ -48,7 +48,10 @@ router.post('/', async(req, res) => {
 router.put('/:id', async(req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.create(req.body);
+    const tagData = await Tag.update(req.body,{
+      where: {
+        id: req.params.id
+  }})
     res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
@@ -58,18 +61,18 @@ router.put('/:id', async(req, res) => {
 router.delete('/:id', async(req, res) => {
   // delete on tag by its `id` value
   try {
-    const categoryData = await Category.destroy({
+    const tagData = await Tag.destroy({
       where: {
         id: req.params.id
       }
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: 'No location found with this id!' });
+      res.status(404).json({ message: 'No Tag Found!' });
       return;
     }
 
-    res.status(200).json(CategoryData);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
